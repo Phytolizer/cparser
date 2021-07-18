cc478c856c844de9909e99c902b647304af36e3a
onContentStop <mariorules24@gmail.com>
2021-07-18 06:17:57 -0500

    import to Git

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#ifdef UNIX
+#include <getopt.h>
+#elif defined(WIN32)
+#include <getopt.h>
+#endif
+
+#include <array>
+#include <fmt/format.h>
+#include <string>
+#include <vector>
+
+constexpr std::array long_opts{
+    option{.name = "file", .has_arg = false, .val = 'f'},
+    option{.name = nullptr, .has_arg = false, .val = -1},
+};
+
+static void printUsage()
+{
+    fmt::print("Usage: {} [options] <FILE...>\n");
+}
+
+std::vector<std::string> parseArgs(int argc, char **argv)
+{
+    int longind;
+    int c;
+    std::vector<std::string> filenames;
+    while ((c = getopt_long(argc, argv, "f:h", long_opts.data(), &longind)) != -1)
+    {
+        switch (c)
+        {
+        case 'f':
+            filenames.emplace_back(optarg);
+            break;
+        case 'h':
+            printUsage();
+            exit(0);
+        default:
+            printUsage();
+            exit(1);
+        }
+    }
+    for (int i = optind; i < argc; ++i)
+    {
+        filenames.emplace_back(argv[i]);
+    }
+
+    return filenames;
+}
+
+int main(int argc, char **argv)
+{
+    std::vector<std::string> filenames = parseArgs(argc, argv);
+    for (const auto &filename : filenames)
+    {
+        compile(filename);
+    }
+    return 0;
+}
